0fcc15ae318d27e1384b1e4c5807e9816f2028b0
강준 <keisoft.kjun@gmail.com>
2023-04-24 23:13:58 +0900

    초기 커밋.

---
+++ FrameTapGestureRecognizerTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FrameTapGestureRecognizerTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FrameTapGestureRecognizerTest/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace FrameTapGestureRecognizerTest
+{
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Properties
+
+        private bool isBusy = true;
+
+        public bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string test;
+
+        public string Test
+        {
+            get
+            {
+                return test;
+            }
+            set
+            {
+                test = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Commands
+        public ICommand MenuClickCommand => new Command(() => OnMenuClickCommand());
+
+        #endregion
+
+        // Constructor
+        #region MainViewModel
+        public MainViewModel()
+        {
+        }
+        #endregion
+
+        // Command Methods
+        #region OnMenuClickCommand
+        private void OnMenuClickCommand()
+        {
+            Application.Current.MainPage.DisplayAlert("메뉴선택", "TapGestureRecognizer Tapped", "OK");
+        }
+        #endregion
+
+        // Methods
+    }
+}
